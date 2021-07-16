7737739adfbb442c0556ed074d928ead1be46ee8
Bruijnesteijn, Bart (ITCDEC) - KLM <bart.bruijnesteijn@klm.com>
2021-07-16 14:16:25 +0200

    Commit

--- Sources/Provider/ProviderDelegate.swift
+++ Sources/Provider/ProviderDelegate.swift
@@ -2,9 +2,13 @@ import SwiftUI
 import UIKit
 
 public protocol ProviderDelegate {
+    init()
+    
     func imageFromAssets(name: String) -> Image
 }
 
 public protocol UIProviderDelegate {
+    init()
+    
     func uiImageFromAssets(name: String) -> UIImage?
 }
